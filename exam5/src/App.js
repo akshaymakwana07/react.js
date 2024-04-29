d40cd092159b6b0d5f70c1c10ab96de21f613981
akshaymakwana07 <akshaymakwana2739@gmail.com>
2024-04-29 17:49:46 +0530

    exam5

--- exam5/src/App.js
+++ exam5/src/App.js
@@ -1,11 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
+import Object from './Object';
+
 
 function App() {
+
+  const array = 
+    [
+    {
+      id : 1,
+      name : "akshay",
+      position : "full stack",
+      salary : 15000
+    },
+
+    {
+      id : 2,
+      name : "pratik",
+      position : "full stack",
+      salary : 17000
+    },
+    {
+      id : 3,
+      name : "vivek",
+      position : "full stack",
+      salary : 15500
+    },
+    {
+      id : 4,
+      name : "nikul",
+      position : "graphic desin",
+      salary : 15000
+    }
+  ]
+  
   return (
    <>
-  <Login/>
+  <Object array= {array}/>
    </>
   );
 }
