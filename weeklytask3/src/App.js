2f2668f356b721aa7692ebb74aaa0da1ae5be099
akshaymakwana07 <akshaymakwana2739@gmail.com>
2024-05-01 18:25:44 +0530

    weeklytask3

---
+++ weeklytask3/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './Movie.css'
+import Form_styling from './Form_styling';
+import Movie from './Movie';
+
+function App() {
+  return (
+    <>
+    {/* <Form_styling /> */}
+
+    <Movie />
+
+    </>
+  );
+}
+
+export default App;
