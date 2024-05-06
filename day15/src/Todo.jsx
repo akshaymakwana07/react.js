e444d6149c382ea2be53e1582b2ea97be7c829ab
akshaymakwana07 <akshaymakwana2739@gmail.com>
2024-05-06 18:31:11 +0530

    day15

---
+++ day15/src/Todo.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+function Todo() {
+
+    const [inputValue,setinputValue] = useState('')
+
+    const [todos,setTodos] = useState ([])
+
+    localStorage.setItem("todo",JSON.stringify(todos))
+
+    function addTask (){
+        let newtodos = {
+            text : inputValue
+        }
+        setTodos([...todos,newtodos])
+        setinputValue('')
+    }
+
+  return (
+    <div>
+      <center>
+      <h1>TODO LIST</h1>
+
+      <input 
+      type='text'
+      placeholder='Enter Your TAsk'
+      value={inputValue}
+      onChange={(e)=>setinputValue(e.target.value)}
+      />
+
+      <button onClick={addTask}>ADD TASK</button>
+      {
+
+todos.map((el,i) => (
+
+<h1 key={i}>{el.text}</h1>
+))
+      }
+      </center>
+    </div>
+
+  )
+}
+
+export default Todo
