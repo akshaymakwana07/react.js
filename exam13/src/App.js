1082e9fd19b33507c788455f7d4e7ac758d338eb
akshaymakwana07 <akshaymakwana2739@gmail.com>
2024-06-20 18:14:41 +0530

    exam13

---
+++ exam13/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Postdata from './Postdata';
+
+function App() {
+  return (
+    <div className="App">
+      <Postdata/>
+    </div>
+  );
+}
+
+export default App;
